e2556ecf2a574d0ea72848bcc55a5605f3dcb9de
vinayak1823 <vinayak18dec@gmail.com>
2021-07-07 18:24:44 +0530

    Added header and display screen

---
+++ frontend/src/screens/Homescreen.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Row, Col } from 'react-bootstrap'
+import Product from '../components/Product'
+import products from '../products'
+const Homescreen = () => {
+    return (
+        <>
+            <h1>Lastest Products</h1>
+            <Row>
+                {products.map((p)=>(
+                    <Col sm={12} md={6} lg={4} xl={3}>
+                        <Product product={p}/>
+                    </Col>
+                ))}
+            </Row>
+        </>
+    )
+}
+
+export default Homescreen
