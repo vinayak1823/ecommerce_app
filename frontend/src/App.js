9654605cad521e15da453d3de3df84da6b54a4c1
vinayak1823 <vinayak18dec@gmail.com>
2021-07-07 18:24:06 +0530

    Added header and display screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,17 @@
-
+import Footer from './components/Footer';
+import Header from './components/Header';
+import {Container} from 'react-bootstrap'
+import Homescreen from './screens/Homescreen';
 function App() {
   return (
     <>
-    <h1>Welcome to Proshop</h1>
+    <Header/>
+    <main className="py-3" >
+      <Container>
+        <Homescreen/>
+      </Container>
+    </main>
+    <Footer/>
     </>
   );
 }
