d528142dc4c082b678f5b889a0e7a832d6c72bc9
vinayak1823 <vinayak18dec@gmail.com>
2021-07-08 16:04:11 +0530

    Added routing and used react-router-bootstrap

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,22 @@
-import Footer from './components/Footer';
-import Header from './components/Header';
-import {Container} from 'react-bootstrap'
-import Homescreen from './screens/Homescreen';
+import Footer from './components/Footer'
+import Header from './components/Header'
+import { Container } from 'react-bootstrap'
+import Homescreen from './screens/Homescreen'
+import { BrowserRouter, Route } from 'react-router-dom'
+import Productscreen from './screens/Productscreen'
 function App() {
   return (
-    <>
-    <Header/>
-    <main className="py-3" >
-      <Container>
-        <Homescreen/>
-      </Container>
-    </main>
-    <Footer/>
-    </>
-  );
+    <BrowserRouter>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <Route path='/' component={Homescreen} exact />
+          <Route path='/product/:id' component={Productscreen} />
+        </Container>
+      </main>
+      <Footer />
+    </BrowserRouter>
+  )
 }
 
-export default App;
+export default App
---
+++ frontend/src/screens/Productscreen.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Productscreen = () => {
+  return <div>Product Screen</div>
+}
+
+export default Productscreen
