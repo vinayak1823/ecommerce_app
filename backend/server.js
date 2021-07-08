d8fbcf7d83082c10ea4382beb630863f87301793
vinayak1823 <vinayak18dec@gmail.com>
2021-07-09 01:23:16 +0530

    Express Setup and routes creation

---
+++ backend/server.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+const products = require('./data/products')
+const port = 5000
+
+app.get('/', (req, res) => {
+  res.send('API is running')
+})
+
+app.get('/api/products', (req, res) => {
+  res.json(products)
+})
+
+app.get('/api/products/:id', (req, res) => {
+  const product = products.find((p) => p._id === req.params.id)
+  res.json(product)
+})
+
+app.listen(port, console.log('Server running'))
