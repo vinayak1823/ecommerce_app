a3e2e696dc04b0976874e327c20d5a522e564d0c
vinayak1823 <vinayak18dec@gmail.com>
2021-07-08 14:53:40 +0530

    Rating component using font awesome

--- frontend/src/screens/Homescreen.js
+++ frontend/src/screens/Homescreen.js
@@ -3,18 +3,18 @@ import { Row, Col } from 'react-bootstrap'
 import Product from '../components/Product'
 import products from '../products'
 const Homescreen = () => {
-    return (
-        <>
-            <h1>Lastest Products</h1>
-            <Row>
-                {products.map((p)=>(
-                    <Col sm={12} md={6} lg={4} xl={3}>
-                        <Product product={p}/>
-                    </Col>
-                ))}
-            </Row>
-        </>
-    )
+  return (
+    <>
+      <h1>Lastest Products</h1>
+      <Row>
+        {products.map((p) => (
+          <Col key={p._id} sm={12} md={6} lg={4} xl={3}>
+            <Product product={p} />
+          </Col>
+        ))}
+      </Row>
+    </>
+  )
 }
 
 export default Homescreen
