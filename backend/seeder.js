d7d63e51f31573be5f192d56daa895c8e6c72aec
vinayak1823 <vinayak18dec@gmail.com>
2021-07-09 20:57:04 +0530

    Added seeder to import data

---
+++ backend/seeder.js
@@ -0,0 +1,53 @@
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+import users from './data/users.js'
+import products from './data/products.js'
+import User from './models/userModel.js'
+import Order from './models/orderModel.js'
+import Product from './models/productModel.js'
+import connectDB from './config/db.js'
+
+dotenv.config()
+connectDB()
+
+const importData = async () => {
+  try {
+    await Order.deleteMany()
+    await User.deleteMany()
+    await Product.deleteMany()
+
+    const createdUsers = await User.insertMany(users)
+    const adminUser = createdUsers[0]._id
+
+    const sampleProducts = products.map((product) => {
+      return { ...product, user: adminUser }
+    })
+
+    await Product.insertMany(sampleProducts)
+
+    console.log('Data imported successfully')
+    process.exit()
+  } catch (error) {
+    console.log(error)
+    process.exit(1)
+  }
+}
+const destroyData = async () => {
+  try {
+    await Order.deleteMany()
+    await User.deleteMany()
+    await Product.deleteMany()
+
+    console.log('Data Destroyed successfully')
+    process.exit()
+  } catch (error) {
+    console.log(error)
+    process.exit(1)
+  }
+}
+
+if (process.argv[2] === '-d') {
+  destroyData()
+} else {
+  importData()
+}
