fc28b6274828b0167b6d48baf8562e8de801038f
vinayak1823 <vinayak18dec@gmail.com>
2021-07-12 16:53:37 +0530

    Created token generator for users

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,6 @@
 import asyncHandler from 'express-async-handler'
 import User from './../models/userModel.js'
-
+import generateToken from '../utils/generateTokens.js'
 // @desc Auth user & get token
 // @route POST /api/users/login
 // @access Public
@@ -14,7 +14,7 @@ const authUser = asyncHandler(async (req, res) => {
       name: user.name,
       email: user.email,
       isAdmin: user.isAdmin,
-      token: null,
+      token: generateToken(user._id),
     })
   } else {
     res.status(401)
