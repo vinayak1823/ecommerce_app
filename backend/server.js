29b1f546f615fe72c3eec5e0566e9f1e79fbdd1e
vinayak1823 <vinayak18dec@gmail.com>
2021-07-12 16:28:37 +0530

    Added the user login route

--- backend/server.js
+++ backend/server.js
@@ -3,17 +3,19 @@ import dotenv from 'dotenv'
 import connectDB from './config/db.js'
 import productRoutes from './routes/productRoutes.js'
 import { notFound, errorHandler } from './middleware/errorMiddleware.js'
+import userRoutes from './routes/userRoutes.js'
 dotenv.config()
 
 connectDB()
 const app = express()
 const port = process.env.PORT || 5000
-
+app.use(express.json())
 app.get('/', (req, res) => {
   res.send('API is running')
 })
 
 app.use('/api/products', productRoutes)
+app.use('/api/users', userRoutes)
 
 app.use(notFound)
 
