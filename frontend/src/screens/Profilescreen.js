11e90c8540b8acdb4f9e75bb6c2b6b4edfa2d441
vinayak1823 <vinayak18dec@gmail.com>
2021-07-15 16:02:11 +0530

    Update user profile

--- frontend/src/screens/Profilescreen.js
+++ frontend/src/screens/Profilescreen.js
@@ -3,7 +3,8 @@ import { Form, Row, Col, Button } from 'react-bootstrap'
 import { useDispatch, useSelector } from 'react-redux'
 import Message from './../components/Message'
 import Loader from './../components/Loader'
-import { getUserDetails } from './../actions/userActions'
+import { getUserDetails, updateUserProfile } from './../actions/userActions'
+import { USER_UPDATE_PROFILE_RESET } from '../constants/userConstants'
 
 const Profilescreen = ({ location, history }) => {
   const [name, setname] = useState('')
@@ -19,25 +20,29 @@ const Profilescreen = ({ location, history }) => {
   const userLogin = useSelector((state) => state.userLogin)
   const { userInfo } = userLogin
 
+  const userUpdateProfile = useSelector((state) => state.userUpdateProfile)
+  const { success } = userUpdateProfile
+
   useEffect(() => {
     if (!userInfo) {
       history.push('/login')
     } else {
-      if (!user.name) {
+      if (!user.name || success) {
+        dispatch({ type: USER_UPDATE_PROFILE_RESET })
         dispatch(getUserDetails('profile'))
       } else {
         setname(user.name)
         setemail(user.email)
       }
     }
-  }, [dispatch, history, userInfo, user])
+  }, [dispatch, history, userInfo, user, success])
 
   function submithandler(e) {
     e.preventDefault()
     if (password !== confirmpassword) {
       setmessage('Passwords do not match')
     } else {
-      //dispatch update profile
+      dispatch(updateUserProfile({ id: user._id, name, email, password }))
     }
   }
 
@@ -47,6 +52,9 @@ const Profilescreen = ({ location, history }) => {
         <h2>User Profile</h2>
         {message && <Message variant='danger'>{message}</Message>}
         {error && <Message variant='danger'>{error}</Message>}
+        {success && (
+          <Message variant='success'>Profile Updated Successfully</Message>
+        )}
         {loading && <Loader />}
         <Form onSubmit={submithandler}>
           <Form.Group controlId='name'>
