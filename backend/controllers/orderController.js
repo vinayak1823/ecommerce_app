49c588a12409f23c59312c1a73e6477b231cfa19
vinayak1823 <vinayak18dec@gmail.com>
2021-07-18 15:44:02 +0530

    Order pay reducer and action in front end

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -55,7 +55,8 @@ const getOrderById = asyncHandler(async (req, res) => {
 const updateOrderToPaid = asyncHandler(async (req, res) => {
   const order = await Order.findById(req.params.id)
   if (order) {
-    ;(order.isPaid = true), (order.paidAt = Date.now())
+    order.isPaid = true
+    order.paidAt = Date.now()
     order.paymentResult = {
       id: req.body.id,
       status: req.body.status,
