7d944558c674630472889db120b7f3e4856eb707
vinayak1823 <vinayak18dec@gmail.com>
2021-07-20 14:56:28 +0530

    Bug fixes for admin/userlist route

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -26,7 +26,11 @@ const Header = () => {
                 </Nav.Link>
               </LinkContainer>
               {userInfo ? (
-                <NavDropdown title={userInfo.name} id='username'>
+                <NavDropdown
+                  title={userInfo.name}
+                  id='username'
+                  className='me-0'
+                >
                   <LinkContainer to='/profile'>
                     <NavDropdown.Item>Profile</NavDropdown.Item>
                   </LinkContainer>
--- frontend/src/screens/UserListscreen.js
+++ frontend/src/screens/UserListscreen.js
@@ -6,15 +6,20 @@ import Loader from './../components/Loader'
 import { listUsers } from './../actions/userActions'
 import { LinkContainer } from 'react-router-bootstrap'
 
-const UserListscreen = () => {
+const UserListscreen = ({ history }) => {
   const dispatch = useDispatch()
 
   const userList = useSelector((state) => state.userList)
   const { loading, error, users } = userList
 
+  const userLogin = useSelector((state) => state.userLogin)
+  const { userInfo } = userLogin
+
   useEffect(() => {
-    dispatch(listUsers())
-  }, [dispatch])
+    if (userInfo && userInfo.isAdmin) {
+      dispatch(listUsers())
+    } else history.push('/login')
+  }, [dispatch, history, userInfo])
 
   const deleteHandler = () => {}
 
