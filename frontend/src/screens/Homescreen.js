72904f13ad3432902eb88d04d605dc43d6b7a4ee
vinayak1823 <vinayak18dec@gmail.com>
2021-07-26 14:45:03 +0530

    Custom page titles

--- frontend/src/screens/Homescreen.js
+++ frontend/src/screens/Homescreen.js
@@ -7,6 +7,8 @@ import Message from '../components/Message'
 import Loader from '../components/Loader'
 import Paginate from './../components/Paginate'
 import ProductCarousel from '../components/ProductCarousel'
+import Meta from './../components/Meta'
+import { Link } from 'react-router-dom'
 
 const Homescreen = ({ match }) => {
   const keyword = match.params.keyword
@@ -22,7 +24,14 @@ const Homescreen = ({ match }) => {
 
   return (
     <>
-      {!keyword && <ProductCarousel />}
+      <Meta />
+      {!keyword ? (
+        <ProductCarousel />
+      ) : (
+        <Link to='/' className='btn btn-light'>
+          Go Back
+        </Link>
+      )}
       <h1>Lastest Products</h1>
       {loading ? (
         <Loader />
--- frontend/src/screens/Productscreen.js
+++ frontend/src/screens/Productscreen.js
@@ -10,6 +10,7 @@ import {
 import Loader from './../components/Loader'
 import Message from './../components/Message'
 import { PRODUCT_CREATE_REVIEW_RESET } from '../constants/productConstants'
+import Meta from './../components/Meta'
 
 const Productscreen = ({ match, history }) => {
   const [qty, setQty] = useState(1)
@@ -64,6 +65,7 @@ const Productscreen = ({ match, history }) => {
         <Message variant='danger'>{error}</Message>
       ) : (
         <>
+          <Meta title={product.name} />
           <Row>
             <Col md={6}>
               <Image src={product.image} alt={product.name} fluid />
